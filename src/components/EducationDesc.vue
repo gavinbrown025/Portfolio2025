e67957e910f325bac9fef4b36d7e3296e7368b59
gavinbrown025 <gavinbrown025@gmail.com>
2025-09-08 12:23:03 +1000

    Added New Project to dev experience

--- src/components/EducationDesc.vue
+++ src/components/EducationDesc.vue
@@ -3,7 +3,6 @@
     <h2 class="mb-0">Education</h2>
     <h3 class="mb-6">{{ education.title }} - {{ education.institution }}</h3>
     <UIMultiParagraph :text="education.description" class="mb-12"/>
-
   </div>
 </template>
 
